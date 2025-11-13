695f5635d2ce4578b8b7fb98a0bb31eda1df1b3c
Przyciok <grabowskiolaf@outlook.com>
2025-11-13 02:17:53 +0100

    Do sprawozdania nie wiem

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ colorama.init()
 if __name__ == "__main__":
 
     liczczasy = True
-    iloscwykonan = 5
+    iloscwykonan = 10
 
     plikBFS = open("plikBFS.txt", "a", encoding='utf-8')
     plikDFS = open("plikDFS.txt", "a", encoding='utf-8')
@@ -25,9 +25,12 @@ if __name__ == "__main__":
     sredniCzasAStar = 0
 
 
-    nazwa_pliku_labiryntu = "macierz_10000x10000.csv"
+    # nazwa_pliku_labiryntu = "macierz_10000x10000.csv"
     # nazwa_pliku_labiryntu = "5x5.csv"
+    nazwa_pliku_labiryntu = "30x30v2.csv"
+
     nazwa_pliku_txt = "rozwiazanie.txt"
+
 
     # Usuwanie pliku z wynikami jeśli istniał
     if os.path.exists(nazwa_pliku_txt):
@@ -62,29 +65,22 @@ if __name__ == "__main__":
             plikBFS.write(f"Liczba przeszukanych pól (odwiedzone + ścieżka): {len(odwiedzone_bfs)} \n")
             plikBFS.write(f"Maksymalna zajętość pamięci (węzły w kolejce): {max_kolejka} \n\n")
 
+            if liczczasy:
+                sredniCzasBFS += czas_bfs
 
-            plikBFS.write("Nowe przejście \n")
-
-
-            # if liczczasy:
-            #     sredniCzasDFS += czas_bfs
-            #
-            #     if czas_bfs < czasBFS or czasBFS == 0:
-            #         czasBFS = czas_bfs
-
-
+                if czas_bfs < czasBFS or czasBFS == 0:
+                    czasBFS = czas_bfs
 
         except (ValueError, FileNotFoundError) as e:
             print(f"Nie udało się uruchomić BFS: {e}")
 
         print("\n" + "=" * 80 + "\n")  # Separator
 
-    # sredniCzasBFS = sredniCzasBFS / iloscwykonan
-    #
-    # plikBFS.write(f"Średni czas wykonania BFS: {sredniCzasBFS} \n")
-    #
-    # plikBFS.write(f"Minimalny czas wykonania: {czasBFS:.6f} s \n")
-    # plikBFS.write("Nowe przejście \n")
+    sredniCzasBFS = sredniCzasBFS / iloscwykonan
+
+    plikBFS.write(f"Średni czas wykonania BFS: {sredniCzasBFS} \n")
+
+    plikBFS.write(f"Minimalny czas wykonania: {czasBFS:.6f} s \n")
 
 
     for i in range(iloscwykonan):
@@ -114,13 +110,11 @@ if __name__ == "__main__":
             plikDFS.write(f"Liczba przeszukanych pól (odwiedzone + ścieżka): {len(odwiedzone_dfs)} \n")
             plikDFS.write(f"Maksymalna zajętość pamięci (węzły w kolejce): {max_stos} \n\n")
 
-            plikBFS.write("Nowe przejście \n")
+            if liczczasy:
+                sredniCzasDFS += czas_dfs
 
-            # if liczczasy:
-            #     sredniCzasDFS += czas_dfs
-            #
-            #     if czas_dfs < czasDFS or czasDFS == 0:
-            #         czasDFS = czas_dfs
+                if czas_dfs < czasDFS or czasDFS == 0:
+                    czasDFS = czas_dfs
 
 
 
@@ -129,53 +123,48 @@ if __name__ == "__main__":
 
         print("\n" + "=" * 80 + "\n")  # Separator
 
-    # sredniCzasDFS = sredniCzasDFS / iloscwykonan
-    # plikDFS.write(f"Średni czas wykonania BFS: {sredniCzasDFS} \n")
-    #
-    # plikDFS.write(f"Minimalny czas wykonania: {czasDFS:.6f} s \n")
-    #
-    # plikBFS.write("Nowe przejście \n")
+    sredniCzasDFS = sredniCzasDFS / iloscwykonan
+    plikDFS.write(f"Średni czas wykonania BFS: {sredniCzasDFS} \n")
 
-    # --- Sekcja A* ---
-    try:
-        print(f"************* Rozpoczynam przeszukiwanie A* (A-gwiazdka) dla {nazwa_pliku_labiryntu} *****************")
-        rozwiazywacz_a_gwiazdka = PrzeszukiwanieAGwiazdka(nazwa_pliku_labiryntu)
+    plikDFS.write(f"Minimalny czas wykonania: {czasDFS:.6f} s \n")
 
-        start_a_gwiazdka = perf_counter()
-        sciezka_a, odwiedzone_a, max_front = rozwiazywacz_a_gwiazdka.rozwiaz()
-        czas_a_gwiazdka = perf_counter() - start_a_gwiazdka
+    for i in range(iloscwykonan):
+        # --- Sekcja A* ---
+        try:
+            print(f"************* Rozpoczynam przeszukiwanie A* (A-gwiazdka) dla {nazwa_pliku_labiryntu} *****************")
+            rozwiazywacz_a_gwiazdka = PrzeszukiwanieAGwiazdka(nazwa_pliku_labiryntu)
 
-        rozwiazywacz_a_gwiazdka.pokaz_wynik(sciezka_a, odwiedzone_a)
+            start_a_gwiazdka = perf_counter()
+            sciezka_a, odwiedzone_a, max_front = rozwiazywacz_a_gwiazdka.rozwiaz()
+            czas_a_gwiazdka = perf_counter() - start_a_gwiazdka
 
-        # Zapisz do pliku
-        # zapisz_rozwiazanie_do_txt(
-        #     nazwa_pliku_txt, "A* (A-gwiazdka)", sciezka_a, odwiedzone_a,
-        #     rozwiazywacz_a_gwiazdka.labirynt, rozwiazywacz_a_gwiazdka.wiersze, rozwiazywacz_a_gwiazdka.kolumny
-        # )
+            rozwiazywacz_a_gwiazdka.pokaz_wynik(sciezka_a, odwiedzone_a)
 
-        print("\n--- Statystyki A* ---")
-        print(f"Czas wykonania: {czas_a_gwiazdka:.6f} s")
-        print(f"Liczba przeszukanych pól (odwiedzone + ścieżka): {len(odwiedzone_a)}")
-        print(f"Maksymalna zajętość pamięci (węzły w kolejce priorytetowej): {max_front}")
+            # Zapisz do pliku
+            # zapisz_rozwiazanie_do_txt(
+            #     nazwa_pliku_txt, "A* (A-gwiazdka)", sciezka_a, odwiedzone_a,
+            #     rozwiazywacz_a_gwiazdka.labirynt, rozwiazywacz_a_gwiazdka.wiersze, rozwiazywacz_a_gwiazdka.kolumny
+            # )
 
-        plikAStar.write(f"Czas wykonania: {czas_a_gwiazdka:.6f} s \n")
-        plikAStar.write(f"Liczba przeszukanych pól (odwiedzone + ścieżka): {len(odwiedzone_a)} \n")
-        plikAStar.write(f"Maksymalna zajętość pamięci (węzły w kolejce): {max_front} \n\n")
+            print("\n--- Statystyki A* ---")
+            print(f"Czas wykonania: {czas_a_gwiazdka:.6f} s")
+            print(f"Liczba przeszukanych pól (odwiedzone + ścieżka): {len(odwiedzone_a)}")
+            print(f"Maksymalna zajętość pamięci (węzły w kolejce priorytetowej): {max_front}")
 
-        plikBFS.write("Nowe przejście \n")
+            plikAStar.write(f"Czas wykonania: {czas_a_gwiazdka:.6f} s \n")
+            plikAStar.write(f"Liczba przeszukanych pól (odwiedzone + ścieżka): {len(odwiedzone_a)} \n")
+            plikAStar.write(f"Maksymalna zajętość pamięci (węzły w kolejce): {max_front} \n\n")
 
-        # if liczczasy:
-        #     sredniCzasAStar += czas_a_gwiazdka
-        #
-        #     if czas_a_gwiazdka < czasAStar or czasAStar == 0:
-        #         czasAStar = czas_a_gwiazdka
+            if liczczasy:
+                sredniCzasAStar += czas_a_gwiazdka
 
-    except (ValueError, FileNotFoundError) as e:
-        print(f"Nie udało się uruchomić A*: {e}")
+                if czas_a_gwiazdka < czasAStar or czasAStar == 0:
+                    czasAStar = czas_a_gwiazdka
 
-    # sredniCzasAStar = sredniCzasAStar / iloscwykonan
-    # plikAStar.write(f"Średni czas wykonania BFS: {sredniCzasAStar} \n")
-    #
-    # plikAStar.write(f"Minimalny czas wykonania: {czasAStar:.6f} s \n")
-    #
-    # plikBFS.write("Nowe przejście \n")
+        except (ValueError, FileNotFoundError) as e:
+            print(f"Nie udało się uruchomić A*: {e}")
+
+    sredniCzasAStar = sredniCzasAStar / iloscwykonan
+    plikAStar.write(f"Średni czas wykonania BFS: {sredniCzasAStar} \n")
+
+    plikAStar.write(f"Minimalny czas wykonania: {czasAStar:.6f} s \n")
